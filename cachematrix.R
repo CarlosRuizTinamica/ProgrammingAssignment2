a33fce400efa22f2c2ee40e0bb8203e179d01b7f
crmTin <carlos.ruiz.tinamica@gmail.com>
2016-09-29 13:37:06 +0200

    First commit for the R programming assignement 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a "CacheMatrix object" from an R matrix.
+## The second function calculates the inverse of the matrix contained in the
+## "CacheMatrix object". If the inverse was already cached, this function 
+## returns the value contained in the "CacheMatrix object"
 
-## Write a short comment describing this function
+## Provided a matrix, it creates a "matrix object" that caches its own inverse. 
+## This object has getters and setter for both the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    
+    ##Return list of get/set methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of x and stores it 
+## into the "cacheMatrix object" x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    
+    ## Return a matrix that is the inverse of 'x'
+    inv
+        
 }
